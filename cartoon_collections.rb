e70f9dba7b7b43fc2ea9f4064c298a04293820c0
Greg Wagner <gpwagner@gmail.com>
2020-01-16 16:22:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+array.each_with_index do |item, index|
+  puts "#{index} #{item}" 
 end
 
 def summon_captain_planet# code an argument here
