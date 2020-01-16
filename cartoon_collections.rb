bc526e39522eab0fcfda264f38cc9564807f72be
Greg Wagner <gpwagner@gmail.com>
2020-01-16 16:33:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,11 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-array.detect {|e| e.length > 4}
+if array.detect {|e| e.length > 4}
+  return true
+else
+  return false
+end
 end
 
 def find_the_cheese# code an argument here
