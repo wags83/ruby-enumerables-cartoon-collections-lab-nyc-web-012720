63c2cbd8aba5e6e708476e3aa13123097204513d
Greg Wagner <gpwagner@gmail.com>
2020-01-16 16:30:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(array)
   array.map{|e| "#{e.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+array.detect {|e| e.length > 4}
 end
 
 def find_the_cheese# code an argument here
